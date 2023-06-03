f8a417a4158f8366ef383193155ec50530c0086e
Lucas Eriksson <132605483+webguru11124@users.noreply.github.com>
2023-06-03 10:16:45 +0900

    update refactored function

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const {deterministicPartitionKey} = require("./refactoredFunction");
 
-console.log(deterministicPartitionKey());
+console.log(deterministicPartitionKey(null));
--- refactoredFunction.js
+++ refactoredFunction.js
@@ -4,10 +4,9 @@ exports.deterministicPartitionKey = (event) => {
     const TRIVIAL_PARTITION_KEY = "0";
     const MAX_PARTITION_KEY_LENGTH = 256;
 
-    let candidate;
-    if (event) candidate = event?.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event) || "").digest("hex");
+    let candidate = event && (event?.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event) || "").digest("hex"));
 
-    if ( typeof candidate !== "string") {
+    if (candidate && typeof candidate !== "string") {
         candidate = JSON.stringify(candidate);
     }
 
